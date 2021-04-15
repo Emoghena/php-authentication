12475d92d5b7eeaf38a9c17f893b80658e319311
Emoghena <52595908+Emoghena@users.noreply.github.com>
2021-04-15 22:33:02 +0100

    Add files via upload

---
+++ reset-password.php
@@ -0,0 +1,39 @@
+<?php
+require_once('functions/inc.php');
+
+if (isset($_SESSION['email'])) {
+    header('location:index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>John Doe Library's Login | Recover password</title>
+</head>
+<body>
+<div class="card">
+<?php
+    if ($_SESSION['errors']) {
+        foreach($_SESSION['errors'] as $error){
+        echo '<div class="errors">'. $error .'</div>';
+        }
+        unset($_SESSION['errors']);
+    }
+?>
+   <form  action="functions/reset-password.php" method="POST">
+      <div class="email-login">
+         <input type="password" placeholder="New password" name="new-password" required autofocus>
+         <label for="confirm-password"> <b>Confirm password</b></label>
+         <input type="password" placeholder="Confirm password" name="confirm-password" required >
+         <input type="hidden" name="email" value="<?=$_GET['email']?>">
+      </div>
+      <button class="cta-btn">Reset password</button>
+     <span class="go-back forget-pass" onclick="window.history.back()">Go back</span>
+   </form>
+</div>
+</body>
+</html>
---
+++ security-question.php
@@ -0,0 +1,44 @@
+<?php
+require_once('functions/inc.php');
+require_once('functions/security-question-func.php');
+
+if (isset($_SESSION['email'])) {
+    header('location:index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>John Doe Library's Recover password</title>
+</head>
+<body>
+<div class="card">
+<?php
+       
+       if ($_SESSION['errors']) {
+          foreach($_SESSION['errors'] as $error){
+           echo '<div class="errors">'. $error .'</div>';
+          }
+          unset($_SESSION['errors']);
+       }
+       ?>
+      
+   <form  action="functions/security-question-func.php" method="POST">
+      <div class="email-login">
+         <label for="email"> <b>Question</b></label>
+         <input type="text"  name="email" value="<?php  echo $question['question']?>" readonly style="background-color:silver; font-weight:bold">
+
+         <label for="email"> <b>Answer</b></label>
+         <input type="text"  name="answer" placeholder="Your answer">
+      </div>
+      <input type="hidden" name="email" value="<?=$_GET['email']?>">
+      <button class="cta-btn">Submit</button>
+     <span class="go-back forget-pass" onclick="window.history.back()">Go back</span>
+   </form>
+</div>
+</body>
+</html>
